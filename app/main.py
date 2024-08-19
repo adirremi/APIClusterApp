9190bc6dc25c2cafa8512dbc2914582fbbf7d033
adirremi <adirremi54@gmail.com>
2024-08-19 05:28:05 +0300

    add oct7list cron job

--- app/main.py
+++ app/main.py
@@ -1,14 +1,33 @@
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from starlette.requests import Request
 import psycopg2
 import os
 
 app = FastAPI()
+templates = Jinja2Templates(directory="app/templates")
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+@app.get("/", response_class=HTMLResponse)
+async def read_table(request: Request):
+    conn = psycopg2.connect(
+        host="34.71.236.251",
+        dbname="mydb",
+        user="user",
+        password="password",
+        port="5432"
+    )
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM fallen_soldiers")
+    rows = cur.fetchall()
+    headers = [desc[0] for desc in cur.description]
+    cur.close()
+    conn.close()
+    
+    return templates.TemplateResponse("table.html", {"request": request, "headers": headers, "rows": rows})
+
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int):
